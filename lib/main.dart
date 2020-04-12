5859c19351890142266862393dbfda8421a9413a
pranto suwarno <17111008@student.mercubuana-yogya.ac.id>
2020-04-12 16:37:06 +0700

    stateful dan stateles eidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int number = 0;
+  void tekanTombol() {
+    
+
+    setState(() {
+      number =number + 1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Container"),
+          title: Text("Stateful widget"),
         ),
-        body: Container(
-          margin: EdgeInsets.all(10),
-          padding: EdgeInsets.all(10),
-          color: Colors.yellowAccent,
-          child: Container(
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                gradient: LinearGradient(
-                    begin: Alignment.bottomRight,
-                    end: Alignment.topLeft,
-                    colors: <Color>[Colors.redAccent, Colors.amberAccent])),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(number.toString()),
+              RaisedButton(
+                child: Text("Add Number"),
+                onPressed: tekanTombol,
+              )
+            ],
           ),
         ),
       ),
