ae414eaddd984b8290330b3637464213cf34dd2b
pranto suwarno <17111008@student.mercubuana-yogya.ac.id>
2020-04-12 14:20:12 +0700

    row dan column

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Halo ini aplikasi pertama"),
+          title: Text("Halo ini Row dan column"),
         ),
-        body: Column()
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text("hello world 1"),
+            Text("hello world 2"),
+            Text("hello world 3"),
+            Row(
+              children: <Widget>[
+                Text("hello world 4"),
+                Text("hello world 5"),
+                Text("hello world 6")
+              ],
+            )
+          ],
+        )
       ),
     );
   }
